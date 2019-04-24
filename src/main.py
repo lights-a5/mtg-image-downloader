5eb8b9f7f4edaadd9762838b0b1b8a54e06526b1
William Willden <srymnir@gmail.com>
2019-04-24 18:10:23 -0400

    Lots of Fixes. Changed to Windows Formatting Temporarily.

--- src/main.py
+++ src/main.py
@@ -12,11 +12,11 @@ class mtg_image_importer:
     def __init__(self, config_path):
         config = configparser.ConfigParser()
         config.read(config_path)
-        self.output_dest = config.get('file_config', 'output_dest') + '/mtg_images'
+        self.output_dest = config.get('file_config', 'output_dest') + '\\mtg_images'
         self._create_folder(self.output_dest)
         self.is_threaded = config.getboolean('thread_config', 'threaded')
         self.threads = config.getint('thread_config', 'max_threads')
-        with open(config.get('file_config', 'json_source')) as card_file:
+        with open(config.get('file_config', 'json_source'), encoding='utf-8') as card_file:
             self.card_list = json.load(card_file)        
 
     def _create_folder(self, directory):
@@ -26,14 +26,12 @@ class mtg_image_importer:
         except OSError:
             print('Error: Creating directory ' + directory)
 
-    def _write_image(self, card_name, card_set, collector_num, response):
+    def _write_image(self, identifier, response):
         if not response.ok:
-            with open('{0}/errors.txt'.format(self.output_dest), 'a') as error_file:
-                error_file.write('{0}_{1}_{2}\n'.format(card_name, card_set, collector_num))
+            with open('{0}\\errors.txt'.format(self.output_dest), 'a') as error_file:
+                error_file.write('{0}\n'.format(identifier))
                 return
-        self._create_folder('{0}/{1}_{2}_{3}'.format(self.output_dest, card_name, card_set, collector_num))
-        with open('{0}/{1}_{2}_{3}/original.png'.format(self.output_dest, card_name, card_set, collector_num), 'wb+') as new_image:
-            print('Writing {0}_{1}_{2}'.format(card_name, card_set, collector_num))
+        with open('{0}\\{1}.png'.format(self.output_dest, identifier), 'wb+') as new_image:
             for block in response.iter_content(1024):
                 if not block:
                     break
@@ -50,31 +48,32 @@ class mtg_image_importer:
             response = requests.get(image_url, stream=True)
         return  response
 
-    def _card_done(self, card_name, card):
-        return os.path.exists('{0}/{1}_{2}_{3}'.format(self.output_dest, card_name, card['set'], card['collector_number']))
+    def _card_done(self, card_identifier):
+        return os.path.exists('{0}\\{1}.png'.format(self.output_dest, card_identifier))
+
 
     def _card_thread(self, card):
         if card['digital'] == True:
-            return 
+            return
+        print("Processing Card # {0} from Set {1}\n".format(card['collector_number'], card['set_name']), end='')
+        identifier = card['id']
         if card['layout'] == 'transform' or card['layout'] == 'double_faced_token':
             for card_face in card['card_faces']:
-                card_name = card_face['name']
-                if self._card_done(card_name, card):
+                identifier += card_face['illustration_id']
+                if self._card_done(identifier):
                     continue
                 response = self.get_image(card_face['image_uris']['png'])
-                self._write_image(card_name, card['set'], card['collector_number'], response)
+                self._write_image(identifier, response)
         else:
-            trantab = str.maketrans('/', '-')
-            card_name = card['name'].translate(trantab)
-            if self._card_done(card_name, card):
+            if self._card_done(identifier):
                 return
             try:
-                image_uri = self.get_image(card['image_uris']['png'])
+                image_uri = card['image_uris']['png']
                 response = self.get_image(image_uri)
-                self._write_image(card_name, card['set'], card['collector_number'], response)
+                self._write_image(identifier, response)
                 
             except KeyError:
-                print(card_name, ' has no image uri associated with it.')
+                print(card['name'] + '_' + card['set'], ' has no image uri associated with it.')
 
     def _import_cards_threaded(self):
         with ThreadPoolExecutor(max_workers=self.threads) as executor:
@@ -94,8 +93,8 @@ class mtg_image_importer:
             self._import_cards_unthreaded()
 
 if __name__ == '__main__':
-    if os.path.exists(os.path.dirname(os.path.realpath(__file__)) + '/config.ini'):
-        importer = mtg_image_importer(os.path.dirname(os.path.realpath(__file__)) + '/config.ini')
+    if os.path.exists(os.path.dirname(os.path.realpath(__file__)) + '\\config.ini'):
+        importer = mtg_image_importer(os.path.dirname(os.path.realpath(__file__)) + '\\config.ini')
     else:
-        importer = mtg_image_importer(os.path.dirname(os.path.realpath(__file__)) + '/example.ini')
+        importer = mtg_image_importer(os.path.dirname(os.path.realpath(__file__)) + '\\example.ini')
     importer.import_cards()
